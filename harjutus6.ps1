a2cbcc30ab6223e6455d48b2367e5fbc0af96ec9
katriin poska <katriin.poska@voco.ee>
2026-03-01 23:39:16 +0200

    harjutus6 csv fail

---
+++ harjutus6.ps1
@@ -0,0 +1,28 @@
+﻿# Harjutus 6 – CSV import ja kooliastme määramine
+
+# Impordi CSV fail
+$students = Import-Csv "students.csv"
+
+# Loo uus tabel koos kooliastmega
+$result = foreach ($s in $students) {
+
+    if ($s.Age -ge 4 -and $s.Age -le 10) {
+        $school = "Junior School"
+    }
+    elseif ($s.Age -ge 11 -and $s.Age -le 17) {
+        $school = "Senior School"
+    }
+    else {
+        $school = "Out of range"
+    }
+
+    # Tagasta uus objekt
+    [PSCustomObject]@{
+        Name   = $s.Name
+        Age    = $s.Age
+        School = $school
+    }
+}
+
+# Kuva tulemus tabelina
+$result | Format-Table -AutoSize
