29863a704c5a8c9968b29e6d143d8f5dbab32778
katriin poska <katriin.poska@voco.ee>
2026-03-01 23:40:45 +0200

    harjutus7

---
+++ harjutus7.ps1
@@ -0,0 +1,23 @@
+﻿# Harjutus 7 – Juhuslikud grupid 20 õpilasele
+
+# Värvigrupid
+$groups = @("Red", "Green", "Yellow", "Blue")
+
+# Lõpptulemus salvestatakse siia
+$result = @()
+
+# Loo 20 õpilast ja määra juhuslik grupp
+for ($i = 1; $i -le 20; $i++) {
+
+    # Vali juhuslik värv
+    $randomGroup = Get-Random -InputObject $groups
+
+    # Lisa tulemus tabelisse
+    $result += [PSCustomObject]@{
+        RollNumber = $i
+        Group      = $randomGroup
+    }
+}
+
+# Kuva tabel
+$result | Format-Table -AutoSize
