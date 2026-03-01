4c138c30a72d296c3c76c5a985789062e84560be
katriin poska <katriin.poska@voco.ee>
2026-03-01 22:42:03 +0200

    harjutus2 hashables

---
+++ harjutus2.ps1
@@ -0,0 +1,25 @@
+﻿# Harjutus 2 – Hashtable arvutused
+
+# Esimene hashtable: mitu päeva iga inimene töötas
+$days = @{
+    John = 12
+    Joe  = 20
+    Mary = 18
+}
+
+# Teine hashtable: palk päevas
+$salary = @{
+    John = 100
+    Joe  = 120
+    Mary = 150
+}
+
+# Kolmas hashtable: kogupalk = DaysWorked * SalaryPerDay
+$total = @{
+    John = $days.John * $salary.John
+    Joe  = $days.Joe  * $salary.Joe
+    Mary = $days.Mary * $salary.Mary
+}
+
+# Väljasta tulemus
+$total
