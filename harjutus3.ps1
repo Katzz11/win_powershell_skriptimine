40686e5030b1d766f9abe465d27344bbdfced880
katriin poska <katriin.poska@voco.ee>
2026-03-01 22:46:29 +0200

    harjutus3 read write host

---
+++ harjutus3.ps1
@@ -0,0 +1,7 @@
+﻿# Harjutus 3 – Read-Host ja Write-Host
+
+# Küsi kasutajalt nimi
+$name = Read-Host "What is your name"
+
+# Kuva nimi rohelise värviga
+Write-Host "Your name is: $name" -ForegroundColor Green
