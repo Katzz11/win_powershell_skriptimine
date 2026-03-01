d54cfcfea8b2ac5ddf6f85bf34aa48d1876acb1d
katriin poska <katriin.poska@voco.ee>
2026-03-01 23:42:35 +0200

    harjutus8 notepadi kontroll

---
+++ harjutus8.ps1
@@ -0,0 +1,8 @@
+﻿# Harjutus 8 – Kontrolli, kas Notepad töötab
+
+while (Get-Process -Name "notepad" -ErrorAction SilentlyContinue) {
+    Write-Host "Notepad is running"
+    Start-Sleep -Seconds 1
+}
+
+Write-Host "All Notepad windows are closed."
