1feb5f73a7ffd98ec62a8e2d61b02ad1024515a5
katriin poska <katriin.poska@voco.ee>
2026-03-01 23:47:19 +0200

    harjutus10 notepad

---
+++ harjutus10.ps1
@@ -0,0 +1,22 @@
+﻿# Harjutus 10 – do until + continue
+
+# Loendur, mitu korda sõnumit näidati
+$count = 0
+
+do {
+    # Kontrolli, kas Notepad töötab
+    $np = Get-Process -Name "notepad" -ErrorAction SilentlyContinue
+
+    # Kui Notepad töötab, kuva sõnum ja jätka tsüklit
+    if ($np) {
+        Write-Host "Notepad is running"
+        $count++
+        Start-Sleep -Seconds 1
+        continue
+    }
+
+}
+until (-not $np)
+
+# Kui tsükkel lõppeb, näita loendurit
+Write-Host $count
